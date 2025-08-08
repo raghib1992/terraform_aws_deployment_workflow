9134aff683b2be2403b0f64ce32818bc722c586c
raghib1992 <65724939+raghib1992@users.noreply.github.com>
2025-08-08 15:50:06 +0530

    Update build_matrix.py

--- scripts/build_matrix.py
+++ scripts/build_matrix.py
@@ -5,27 +5,27 @@ def get_account_info(env):
     # Map environment to AWS account details
     account_map = {
         "sandbox": {
-            "account_id": "271547279005",
+            "account_id": "800643485173",
             "account_name": "dev-account"
         },
         "dev": {
-            "account_id": "XXXXXXXXXXXX",
+            "account_id": "254390148077",
             "account_name": "dev-account"
         },
         "qa": {
-            "account_id": "222222222222",
+            "account_id": "039871571616",
             "account_name": "qa-account"
         },
         "performance": {
-            "account_id": "333333333333",
+            "account_id": "677977426648",
             "account_name": "performance-account"
         },
         "staging": {
-            "account_id": "XXXXXXXXXXXX",
+            "account_id": "649128807268",
             "account_name": "staging-account"
         },
         "staging-green": {
-            "account_id": "XXXXXXXXXXXX",
+            "account_id": "606184927985",
             "account_name": "staging-green-account"
         },
         "production": {
@@ -60,7 +60,7 @@ def main():
             "env": env,
             "account_id": account_info["account_id"],
             "account_name": account_info["account_name"],
-            "path": f"./{sub}"
+            "path": f"{sub}"
         })
 
     print(json.dumps(matrix))  # Output to stdout
