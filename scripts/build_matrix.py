3c78099b6fcf02ff3178a9494b4a1adc2e5409b9
raghib1992 <65724939+raghib1992@users.noreply.github.com>
2025-08-12 16:21:34 +0530

    Update build_matrix.py

--- scripts/build_matrix.py
+++ scripts/build_matrix.py
@@ -6,26 +6,32 @@ def get_account_info(env):
     account_map = {
         "sandbox": {
             "account_id": "800643485173",
+            "bucket_name": "sandbox-terraform-state",
             "account_name": "dev-account"
         },
         "dev": {
             "account_id": "254390148077",
+            "bucket_name": "dev-terraform-state",
             "account_name": "dev-account"
         },
         "qa": {
             "account_id": "039871571616",
+            "bucket_name": "qa-terraform-state",
             "account_name": "qa-account"
         },
         "performance": {
             "account_id": "677977426648",
+            "bucket_name": "performance-terraform-state",
             "account_name": "performance-account"
         },
         "staging": {
             "account_id": "649128807268",
+            "bucket_name": "staging-terraform-state",
             "account_name": "staging-account"
         },
         "staging-green": {
             "account_id": "606184927985",
+            "bucket_name": "staging-green-terraform-state",
             "account_name": "staging-green-account"
         },
         "production": {
@@ -58,7 +64,8 @@ def main():
         matrix.append({
             "subdomain": sub,
             "env": env,
-            "env_name": f"apply-{sub}-{env}",
+            "env_name": f"{env}-approval",
+            "bucket_name": account_info["bucket_name"],
             "account_id": account_info["account_id"],
             "account_name": account_info["account_name"],
             "path": f"{sub}"
